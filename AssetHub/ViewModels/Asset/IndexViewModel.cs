9f0506f0e0f7c11051d06a1825330d7e999b9b1b
Jan Kelemen <jkeleme@gmail.com>
2016-04-20 16:53:36 +0200

    some work on asset/index

---
+++ AssetHub/ViewModels/Asset/IndexViewModel.cs
@@ -0,0 +1,37 @@
+﻿using AssetHub.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssetHub.ViewModels.Asset
+{
+    public class IndexViewModel
+    {
+        AssetHubContext db = new AssetHubContext();
+
+        public IndexViewModel()
+        {
+            SelectedAssetModel = -1;
+            SelectedCategory = -1;
+            SearchResults = new List<Models.Asset>();
+        }
+
+        public string Name { get; set; }
+
+        [Display(Name ="Serial Key")]
+        public string SerialKey { get; set; }
+
+        public int SelectedAssetModel { get; set; }
+
+        public IEnumerable<SelectListItem> AssetModels { get; set; }
+
+        public int SelectedCategory { get; set; }
+
+        public IEnumerable<SelectListItem> Categories { get; set; }
+
+        public List<Models.Asset> SearchResults { get; set; }
+    }
+}
