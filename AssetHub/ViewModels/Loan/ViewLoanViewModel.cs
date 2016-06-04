d7b775ef68837485f966accc04f9fb2ce1006dbc
Jan Kelemen <jkeleme@gmail.com>
2016-06-04 23:31:06 +0200

    update diagrams

---
+++ AssetHub/ViewModels/Loan/ViewLoanViewModel.cs
@@ -0,0 +1,18 @@
+﻿using AssetHub.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AssetHub.ViewModels.Loan
+{
+    public class ViewLoanViewModel
+    {
+        AssetHubContext db = new AssetHubContext();
+
+        public ViewLoanViewModel(int id)
+        {
+
+        }
+    }
+}
